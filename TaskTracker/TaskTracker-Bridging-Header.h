0982c56b91763c08ddaf09e26ebaefd855228c0b
Wiktor Gancarz <victor.gancarz@gmail.com>
2018-09-27 15:22:00 +0100

    Initial Commit

---
+++ TaskTracker/TaskTracker-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  TaskTracker-Bridging-Header.h
+//  TaskTracker
+//
+//  Created by Wiktor Gancarz on 27/09/2018.
+//  Copyright © 2018 SMYK Business Solution Ltd. All rights reserved.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
